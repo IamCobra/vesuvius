b72dbf6af48eb45295ee364cac4af296f28dd623
Ikhvans Mac <ikhvan95@gmail.com>
2025-10-08 00:22:29 +0200

    unødvendige dele af kommentare

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,16 @@
 import { withAuth } from "next-auth/middleware";
 
-export default withAuth(
-  function middleware(req) {
-    // Tilføj ekstra logik her hvis nødvendigt
-  },
-  {
-    callbacks: {
-      authorized: ({ token, req }) => {
-        // Check hvis ruten kræver admin access - KUN ADMIN
-        if (req.nextUrl.pathname.startsWith("/admin")) {
-          return token?.role === "ADMIN";
-        }
+export default withAuth(function middleware(req) {}, {
+  callbacks: {
+    authorized: ({ token, req }) => {
+      if (req.nextUrl.pathname.startsWith("/admin")) {
+        return token?.role === "ADMIN";
+      }
 
-        // For andre beskyttede ruter, kræv bare at bruger er logget ind
-        return !!token;
-      },
+      return !!token;
     },
-  }
-);
+  },
+});
 
 export const config = {
   matcher: ["/admin/:path*"],
